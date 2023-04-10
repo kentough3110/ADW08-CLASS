089a42554b6d884a5ab33ac0aecf5cfec448cf28
kentough3110 <120083240+kentough3110@users.noreply.github.com>
2023-04-10 13:31:21 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvProdukSimpan = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tShirtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shirtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.longPantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shortsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelSubtotal = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdukSimpan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tShirtToolStripMenuItem,
+            this.shirtToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(68, 20);
+            this.toolStripMenuItem1.Text = "Top Wear";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.longPantsToolStripMenuItem,
+            this.shortsToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(89, 20);
+            this.toolStripMenuItem2.Text = "Bottom Wear";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(80, 20);
+            this.toolStripMenuItem3.Text = "Accessories";
+            // 
+            // dgvProdukSimpan
+            // 
+            this.dgvProdukSimpan.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProdukSimpan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProdukSimpan.Location = new System.Drawing.Point(483, 27);
+            this.dgvProdukSimpan.Name = "dgvProdukSimpan";
+            this.dgvProdukSimpan.Size = new System.Drawing.Size(317, 296);
+            this.dgvProdukSimpan.TabIndex = 1;
+            this.dgvProdukSimpan.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProdukSimpan_CellValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sub-Total :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(657, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Total :";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.usgoods_68_456772;
+            this.pictureBox3.Location = new System.Drawing.Point(327, 43);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(126, 183);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.usgoods_56_422992;
+            this.pictureBox2.Location = new System.Drawing.Point(170, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(126, 183);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.usgoods_55_455365;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "T-Shirt Kerah Bulat";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "T-Shirt Kerah Bulat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(324, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "AIRsm Katun";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Rp. 150.000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(167, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Rp. 150.000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(324, 264);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Rp. 200.000";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add To Cart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Add To Cart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(327, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Add To Cart";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tShirtToolStripMenuItem
+            // 
+            this.tShirtToolStripMenuItem.Name = "tShirtToolStripMenuItem";
+            this.tShirtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tShirtToolStripMenuItem.Text = "T-Shirt";
+            this.tShirtToolStripMenuItem.Click += new System.EventHandler(this.tShirtToolStripMenuItem_Click);
+            // 
+            // shirtToolStripMenuItem
+            // 
+            this.shirtToolStripMenuItem.Name = "shirtToolStripMenuItem";
+            this.shirtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shirtToolStripMenuItem.Text = "Shirt";
+            this.shirtToolStripMenuItem.Click += new System.EventHandler(this.shirtToolStripMenuItem_Click);
+            // 
+            // longPantsToolStripMenuItem
+            // 
+            this.longPantsToolStripMenuItem.Name = "longPantsToolStripMenuItem";
+            this.longPantsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.longPantsToolStripMenuItem.Text = "Long Pants";
+            // 
+            // shortsToolStripMenuItem
+            // 
+            this.shortsToolStripMenuItem.Name = "shortsToolStripMenuItem";
+            this.shortsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shortsToolStripMenuItem.Text = "Shorts";
+            // 
+            // labelSubtotal
+            // 
+            this.labelSubtotal.AutoSize = true;
+            this.labelSubtotal.Location = new System.Drawing.Point(701, 336);
+            this.labelSubtotal.Name = "labelSubtotal";
+            this.labelSubtotal.Size = new System.Drawing.Size(0, 13);
+            this.labelSubtotal.TabIndex = 16;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(700, 364);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.TabIndex = 17;
+            this.lblTotal.Text = "?";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // lblSubTotal
+            // 
+            this.lblSubTotal.AutoSize = true;
+            this.lblSubTotal.Location = new System.Drawing.Point(700, 336);
+            this.lblSubTotal.Name = "lblSubTotal";
+            this.lblSubTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblSubTotal.TabIndex = 18;
+            this.lblSubTotal.Text = "?";
+            this.lblSubTotal.Click += new System.EventHandler(this.lblSubTotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSubTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.labelSubtotal);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvProdukSimpan);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProdukSimpan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.DataGridView dgvProdukSimpan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem tShirtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shirtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem longPantsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shortsToolStripMenuItem;
+        private System.Windows.Forms.Label labelSubtotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblSubTotal;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        DataTable dtProdukSimpan = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            dtProdukSimpan.Columns.Add("Item");
+            dtProdukSimpan.Columns.Add("Qty");
+            dtProdukSimpan.Columns.Add("Price");
+            dtProdukSimpan.Columns.Add("Total");
+            dgvProdukSimpan.DataSource = dtProdukSimpan; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (dtProdukSimpan.Rows.Count <= 0)
+            {
+                dtProdukSimpan.Rows.Add("T-Shirt Kerah Bulat", "1", "150000", "150000");
+
+            }
+            else
+            {
+                bool isAvail = false;
+                foreach (DataRow item in dtProdukSimpan.Rows)
+                {
+                    isAvail = true;
+                    if (item[0].ToString() == "T-Shirt Kerah Bulat")
+                    {
+                        item[1] = Convert.ToInt32(item[1].ToString())+1;
+                        item[2] = Convert.ToInt32(item[2].ToString()) + 150000;
+                        item[3] = Convert.ToInt32(item[1].ToString()) * Convert.ToInt32(item[2].ToString());
+                    }
+                }
+                if (!isAvail)
+                {
+                    dtProdukSimpan.Rows.Add("T-Shirt Kerah Bulat", "1", "150000", "150000");
+                }
+            }
+            dgvProdukSimpan.DataSource = dtProdukSimpan;
+            lblSubTotal_Click(sender, e);
+            lblTotal_Click(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dtProdukSimpan.Rows.Count <= 0)
+            {
+                dtProdukSimpan.Rows.Add("T-Shirt Kerah Bulat", "1", "150000", "150000");
+
+            }
+            else
+            {
+                bool isAvail = false;
+                foreach (DataRow item in dtProdukSimpan.Rows)
+                {
+                    isAvail = true;
+                    if (item[0].ToString() == "T-Shirt Kerah Bulat")
+                    {
+                        item[1] = Convert.ToInt32(item[1].ToString()) + 1;
+                        item[2] = Convert.ToInt32(item[2].ToString()) + 150000;
+                        item[3] = Convert.ToInt32(item[1].ToString()) * Convert.ToInt32(item[2].ToString());
+                    }
+                }
+                if (!isAvail)
+                {
+                    dtProdukSimpan.Rows.Add("T-Shirt Kerah Bulat", "1", "150000", "150000");
+                }
+            }
+            dgvProdukSimpan.DataSource = dtProdukSimpan;
+            lblSubTotal_Click(sender, e);
+            lblTotal_Click(sender, e);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dtProdukSimpan.Rows.Count <= 0)
+            {
+                dtProdukSimpan.Rows.Add("AIRsm Katun", "1", "200000", "200000");
+
+            }
+            else
+            {
+                bool isAvail = false;
+                foreach (DataRow item in dtProdukSimpan.Rows)
+                {
+                    if (item[0].ToString() == "AIRsm Katun")
+                    {
+                        isAvail = true;
+                        item[1] = Convert.ToInt32(item[1].ToString()) + 1;
+                        item[2] = Convert.ToInt32(item[2].ToString()) + 200000;
+                        item[3] = Convert.ToInt32(item[1].ToString()) * Convert.ToInt32(item[2].ToString());
+                    }
+                }
+                if (!isAvail)
+                {
+                    dtProdukSimpan.Rows.Add("AIRsm Katun", "1", "200000", "200000");
+                }
+            }
+            dgvProdukSimpan.DataSource = dtProdukSimpan;
+            lblSubTotal_Click(sender, e);
+            lblTotal_Click(sender, e);
+        }
+
+        private void tShirtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void shirtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void lblSubTotal_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i < dgvProdukSimpan.Rows.Count; i++)
+            {
+                sum += Convert.ToInt32(dgvProdukSimpan.Rows[i].Cells[3].Value);
+            }
+            lblSubTotal.Text = sum.ToString();
+        }
+
+        private void lblTotal_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            double tax = 0.10;
+            for (int i = 0; i < dgvProdukSimpan.Rows.Count; i++)
+            {
+                sum += Convert.ToInt32(dgvProdukSimpan.Rows[i].Cells[3].Value);
+            }
+            double sumTax = sum * tax;
+            sum = sum + sumTax;
+            lblTotal.Text = sum.ToString();
+        }
+
+        private void dgvProdukSimpan_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            
+            dtProdukSimpan.Rows[e.RowIndex][3] = (Convert.ToInt32(dtProdukSimpan.Rows[e.RowIndex][1]) * Convert.ToInt32(dtProdukSimpan.Rows[e.RowIndex][3]));
+            
+            lblSubTotal_Click(sender, e);
+            lblTotal_Click(sender, e);
+        }
+    }
+}
